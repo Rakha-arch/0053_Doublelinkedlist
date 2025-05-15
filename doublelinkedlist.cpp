5405400da02a9a7bafc8c25f9a845b7f8849cb65
Rakha-arch <miftahuzahran@gmail.com>
2025-05-15 11:42:47 +0700

    commit 1

---
+++ doublelinkedlist.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+    public :
+    int nomMhs;
+    Node *next;
+    Node *prev;
+};
+
+class Doublelinkedlist
+{
+    private:
+        Node *START;
+
+    public:
+        Doublelinkedlist()
+        {
+            START = NULL;
+        }
+
+        void addNode()
+        {
+            int nim;
+            string nm;
+            cout << "\nEnter the roll number of the student";
+            cin >> nim;
+
+          
+
+           
+
+            
+            {
+                if (START != NULL && nim == START ->nomMhs)
+                {
+                   
+                }
+               
+
+
+              
+
+               
+            }
+
+          
+            {
+                current = current->next;
+            }
+
+            {
+                
+            }
+
+         
+
+        }
+
+        void hapus()
+        {
+            if (START == NULL)
+            {
+               
+            }
+
+           
+
+           
+            {
+             
+            }
+
+            {
+               
+            }
+            else
+            {
+                
+
+            }
+
+            
+        }
+
+        void traverse()
+        {
+          
+            {
+        
+            }
+           
+
+          
+            {
+         
+            }
+
+        }
+
+        void revtraverse()
+        {
+           
+            {
+              
+            }
+      
+            {
+                currentNode = currentNode->next;
+                i++;
+            }
+            //step2 : Traverse backward
+            cout << "\nrecording  "
+        }
+}
